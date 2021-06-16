9ea1d6d0c9ccaee0d414f0f9b3673af230983128
Zagii <zagawus@interia.pl>
2021-06-16 23:55:53 +0200

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,48 +1,56 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from "./app.component";
-import { HelloComponent } from "./hello.component";
-import { ChatService } from "./chat.service";
-import { WsServiceService } from "./ws-service.service";
-import { PairDataComponent } from "./pair-data/pair-data.component";
-import { RestService } from "./rest.service";
-import { HttpClientModule } from "@angular/common/http";
-import { DebugComponent } from "./debug/debug.component";
-import { EkranStartComponent } from "./ekran-start/ekran-start.component";
-import { Routes, RouterModule } from "@angular/router";
-import ekranStartComponentCss from "./ekran-start/ekran-start.component.css";
-import { ParaComponent } from "./para/para.component";
-import { EkranParaComponent } from "./ekran-para/ekran-para.component";
-import { WykresComponent } from "./wykres/wykres.component";
-import { NgxEchartsModule } from "ngx-echarts";
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { ChatService } from './chat.service';
+import { WsServiceService } from './ws-service.service';
+import { PairDataComponent } from './pair-data/pair-data.component';
+import { RestService } from './rest.service';
+import { HttpClientModule } from '@angular/common/http';
+import { DebugComponent } from './debug/debug.component';
+import { EkranStartComponent } from './ekran-start/ekran-start.component';
+import { Routes, RouterModule } from '@angular/router';
+import ekranStartComponentCss from './ekran-start/ekran-start.component.css';
+import { ParaComponent } from './para/para.component';
+import { EkranParaComponent } from './ekran-para/ekran-para.component';
+import { WykresComponent } from './wykres/wykres.component';
+import { NgxEchartsModule } from 'ngx-echarts';
 
-import * as echarts from "echarts";
-import { Wykres1minComponent } from "./wykres1min/wykres1min.component";
-import { EkranDebugComponent } from "./ekran-debug/ekran-debug.component";
+import * as echarts from 'echarts';
+import { Wykres1minComponent } from './wykres1min/wykres1min.component';
+import { EkranDebugComponent } from './ekran-debug/ekran-debug.component';
 import { KalkulacjeService } from './kalkulacje.service';
 import { WykresDataComponent } from './wykres-data/wykres-data.component';
 import { EkranAutomatComponent } from './ekran-automat/ekran-automat.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+
 const routes: Routes = [
-  { path: "home", component: EkranStartComponent },
-  { path: "para/:id", component: EkranParaComponent },
-  { path: "debug/:id", component: EkranDebugComponent },
-  { path: "automat/:id", component: EkranAutomatComponent },
-  { path: "", redirectTo: "/home", pathMatch: "full" },
-  { path: "**", redirectTo: "/home" }
+  { path: 'home', component: EkranStartComponent },
+  { path: 'para/:id', component: EkranParaComponent },
+  { path: 'debug/:id', component: EkranDebugComponent },
+  { path: 'automat/:id', component: EkranAutomatComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' }
 ];
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+
     HttpClientModule,
     RouterModule.forRoot(routes, { useHash: true }),
     NgxEchartsModule.forRoot({
-      echarts: () => import("echarts")
-    })
+      echarts: () => import('echarts')
+    }),
+    BrowserAnimationsModule,
+    TooltipModule.forRoot(),
+    BsDropdownModule.forRoot()
   ],
   declarations: [
     AppComponent,
