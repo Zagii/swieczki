729a365d6cb4e588d989e201ea388fc7a156b78b
Zagii <zagawus@interia.pl>
2021-06-25 00:36:45 +0200

    2 files modified

--- src/app/ekran-automat/ekran-automat.component.ts
+++ src/app/ekran-automat/ekran-automat.component.ts
@@ -13,8 +13,27 @@ export class EkranAutomatComponent implements OnInit, OnDestroy {
   symbol: string;
   swieczki15m: Candle[];
   swieczki1m: Candle[];
-
+  dropdownOkres = [
+    { opis: '1x15min= 15min', wartosc: 1 },
+    { opis: '2x15min= 30min', wartosc: 2 },
+    { opis: '3x15min= 45min', wartosc: 3 },
+    { opis: '5x15min= 1h 15', wartosc: 5 },
+    { opis: '8x15min= 2h', wartosc: 8 },
+    { opis: '13x15min= 3h 15min', wartosc: 13 },
+    { opis: '21x15min=5h 15min', wartosc: 21 },
+    { opis: '34x15min= 8h 30min', wartosc: 34 },
+    { opis: '55x15min= 13h 45min', wartosc: 55 },
+    { opis: '89x15min= 22h 15min', wartosc: 89 },
+    { opis: '144x15min= 1d 12h', wartosc: 144 }
+  ];
+  defOkresIndex = 5;
+  wybranyOkres: number;
+  count15min: number;
   calcObj: KalkulacjeObjct;
+
+  deltaSL: number;
+  poczatekSL:number;
+  przyrostSL:number;
 
   private calcObjSub: Subject<KalkulacjeObjct>;
 
@@ -24,7 +43,9 @@ export class EkranAutomatComponent implements OnInit, OnDestroy {
   constructor(
     private route: ActivatedRoute,
     private kalkulacje: KalkulacjeService
-  ) {}
+  ) {
+    this.zmienOkres(this.defOkresIndex);
+  }
 
   ngOnDestroy(): void {
     this.candle1mSubs.unsubscribe();
@@ -46,5 +67,19 @@ export class EkranAutomatComponent implements OnInit, OnDestroy {
     this.candle15mSubs.subscribe(x => (this.swieczki15m = x));
     this.calcObjSub = this.kalkulacje.getCalcObjSubj();
     this.calcObjSub.subscribe(x => (this.calcObj = x));
+  }
+  zmienOkres(index) {
+    this.wybranyOkres = index;
+    this.count15min = this.dropdownOkres[this.wybranyOkres].wartosc;
+    console.log('Nowy okres: ' + this.count15min + ' x 15min swieczek');
+  }
+  updateOkres(e) {
+    let i = e.target.value;
+    // console.log(e.target);
+    // console.log('Update: ' + i);
+    this.zmienOkres(i);
+  }
+  changeSL() {
+    console.log('changeSL' + this.deltaSL);
   }
 }
